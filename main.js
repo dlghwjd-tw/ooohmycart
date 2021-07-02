e78a56212ef72f2a0bfc49e41be3e0ef554f91be
dlghwjd-tw <83802103+dlghwjd-tw@users.noreply.github.com>
2021-07-02 19:13:05 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var bodyEle = document.body
 
 var maintainenceScreen = document.createElement('div')
+maintainenceScreen.style.position = 'absolute'
 maintainenceScreen.style.height = '1vh'
 maintainenceScreen.style.width = '1vw'
 maintainenceScreen.style.textAlign = 'center'

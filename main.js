c4a801a976413a63d79d4849ddab7a46847395cb
dlghwjd-tw <83802103+dlghwjd-tw@users.noreply.github.com>
2021-07-14 18:02:50 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 var bodyEle = document.body
 
-var maintainenceScreen = document.createElement('div')
-maintainenceScreen.style.position = 'absolute'
-maintainenceScreen.style.height = '1vh'
-maintainenceScreen.style.width = '1vw'
-maintainenceScreen.style.textAlign = 'center'
-maintainenceScreen.style.background = '#FFFFFF'
-maintainenceScreen.innerHTML = '網站維護中...'
-bodyEle.prepend(maintainenceScreen)
+// Maintenance Screen
+var maintenanceScreen = document.createElement('div')
+maintenanceScreen.style.position = 'absolute'
+maintenanceScreen.style.height = '1vh'
+maintenanceScreen.style.width = '1vw'
+maintenanceScreen.style.textAlign = 'center'
+maintenanceScreen.style.background = '#FFFFFF'
+maintenanceScreen.innerHTML = '網站維護中...'
+bodyEle.prepend(maintenanceScreen)
+
+// Adjust menu buttons
+var homeBtnEle = document.querySelector('[onclick="bt_onclick(\'/ooohmycarthk/home\',\'sub23\');"]')
+homeBtnEle.parentNode.removeChild(homeBtnEle)
+var homeBtnReplacement = document.createElement('div')
+homeBtnReplacement.innerHTML = '首頁'
+homeBtnReplacement.style.cursor = 'pointer'
+homeBtnReplacement.onclick = 'bt_onclick(\'/ooohmycarthk/home\',\'sub23\');'
+homeBtnEle.parentNode.append(homeBtnReplacement)

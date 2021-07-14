4f57b763d381bf0e371da14139225979aa5ca09a
dlghwjd-tw <83802103+dlghwjd-tw@users.noreply.github.com>
2021-07-14 18:04:47 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 var bodyEle = document.body
+var webStatus
 
 // Maintenance Screen
-var maintenanceScreen = document.createElement('div')
-maintenanceScreen.style.position = 'absolute'
-maintenanceScreen.style.height = '1vh'
-maintenanceScreen.style.width = '1vw'
-maintenanceScreen.style.textAlign = 'center'
-maintenanceScreen.style.background = '#FFFFFF'
-maintenanceScreen.innerHTML = '網站維護中...'
-bodyEle.prepend(maintenanceScreen)
+if (webStatus == 'underMaintenance') {
+    var maintenanceScreen = document.createElement('div')
+    maintenanceScreen.style.position = 'fixed'
+    maintenanceScreen.style.height = '1vh'
+    maintenanceScreen.style.width = '1vw'
+    maintenanceScreen.style.textAlign = 'center'
+    maintenanceScreen.style.background = '#FFFFFF'
+    maintenanceScreen.innerHTML = '網站維護中...'
+    bodyEle.prepend(maintenanceScreen)
+}
 
 // Adjust menu buttons
 var homeBtnEle = document.querySelector('[onclick="bt_onclick(\'/ooohmycarthk/home\',\'sub23\');"]')
